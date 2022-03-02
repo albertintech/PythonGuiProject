ae27fbf457a954f0112151b99c2824d53a9a5ff3
Albert Ramos <albertramos.dev@gmail.com>
2022-03-02 10:29:49 -0700

    Example class

--- oop-distance-converter/app.py
+++ oop-distance-converter/app.py
@@ -1,1 +1,21 @@
 """OOP Distance Converter"""
+import tkinter as tk
+from tkinter import ttk
+
+# for high dpi monitors on Windows only
+try:
+    from ctypes import windll
+    windll.shcore.SetProcessDpiAwareness(1)
+except:
+    pass
+
+class HelloWorld(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Hello, World!")
+
+        ttk.Label(self, text="Hello, World!").pack()
+
+root = HelloWorld()
+root.mainloop()
